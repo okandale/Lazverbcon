707f71e53294095f28250ffce83962d3995f64cf
lewisccz <lewiscancharles@gmail.com>
2024-08-07 20:56:48 +0100

    host on local network

--- laz_verb_conjugator/frontend/vite.config.js
+++ laz_verb_conjugator/frontend/vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Listen on all addresses
+    port: 5173,
     proxy: {
       '/conjugate': 'http://localhost:5000'
     }
+    
   }
 })
