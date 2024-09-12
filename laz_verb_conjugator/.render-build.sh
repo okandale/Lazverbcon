f089478b804972113e133b2d545276a255436ae2
okandale <okan.dale@outlook.com>
2024-09-12 15:12:18 -0400

    render-build update

--- laz_verb_conjugator/.render-build.sh
+++ laz_verb_conjugator/.render-build.sh
@@ -1,27 +1,4 @@
 #!/usr/bin/env bash
 
-# Print the current directory for debugging
-echo "Current directory: $(pwd)"
-
-# Navigate to the frontend directory
-echo "Navigating to frontend..."
-cd frontend
-
-# Install frontend dependencies
-echo "Installing npm dependencies..."
 npm install
-
-# Build the React frontend
-echo "Building the frontend..."
-npm run-script build
-
-# Navigate back to the root
-cd ..
-
-# Install Python dependencies using pip
-echo "Installing Python dependencies with pip..."
-pip install -r requirements.txt
-
-# Start the Flask app using gunicorn, binding to the correct Render port
-echo "Starting gunicorn..."
-gunicorn app:app --bind 0.0.0.0:$PORT
+npm run build
