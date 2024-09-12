fe951ed67000d0bcdc451e45204c021d35aafd1f
lewisccz <lewiscancharles@gmail.com>
2024-09-12 20:56:26 +0100

    minor changes

--- laz_verb_conjugator/frontend/vite.config.js
+++ laz_verb_conjugator/frontend/vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Listen on all addresses
-    port: 5173,
-    proxy: {
-      '/conjugate': 'http://localhost:5000'
-    }
-    
+  build: {
+    outDir: 'dist', // Ensure this matches the directory you are deploying from
   }
 })
