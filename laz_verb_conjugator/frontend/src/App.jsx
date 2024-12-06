121499b8834f0559df842ae4babae5172e821983
lewisccz <lewiscancharles@gmail.com>
2024-12-06 15:32:52 +0000

    remove login page

--- laz_verb_conjugator/frontend/src/App.jsx
+++ laz_verb_conjugator/frontend/src/App.jsx
@@ -1,20 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/login';
 import VerbConjugator from './components/VerbConjugator';
-import VerbList from './components/VerbList'; // Correct import
+import VerbList from './components/VerbList';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/conjugator" element={<VerbConjugator />} />
-        <Route path="/verbs" element={<VerbList />} /> {/* New route */}
+        <Route path="/" element={<VerbConjugator />} />
+        <Route path="/verbs" element={<VerbList />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
