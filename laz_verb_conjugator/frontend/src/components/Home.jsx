fef88ce18a74c3d66acec194cd0e670f742a812d
lewisccz <lewiscancharles@gmail.com>
2024-12-30 16:47:49 +0000

    remove home link from homepage

--- laz_verb_conjugator/frontend/src/components/Home.jsx
+++ laz_verb_conjugator/frontend/src/components/Home.jsx
@@ -16,10 +16,7 @@ const HomePage = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <div className="flex justify-between items-center mb-8 pt-2">
-        <Link to="/" className="text-gray-600 hover:text-gray-800">
-          <Home size={24} />
-        </Link>
+      <div className="flex justify-end items-center mb-8 pt-2">
         <LanguageToggle language={language} onToggle={toggleLanguage} />
       </div>
 
