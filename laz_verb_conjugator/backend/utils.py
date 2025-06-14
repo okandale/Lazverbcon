f1219f2ab204fcc76123e3e0f222d418f98a81ae
okandale <okan.dale@outlook.com>
2025-06-14 06:35:23 -0400

    changed phonetic rule v to include m under m

--- laz_verb_conjugator/backend/utils.py
+++ laz_verb_conjugator/backend/utils.py
@@ -75,7 +75,7 @@ def get_phonetic_rules(region: str, is_tvm: bool = False) -> tuple:
                 'p': ['t', 'k', 'ʒ', 'ç', 'f', 's', 'ş', 'x', 'h'],
                 'b': ['a', 'e', 'i', 'o', 'u', 'd', 'g', 'ž', 'c', 'v', 'z', 'j', 'ğ'],
                 'p̌': ['ç̌', 'ǩ', 'q', 'ǯ', 't̆'],
-                'm': ['b', 'n']
+                'm': ['b', 'n', 'm']
             }
         else:
             phonetic_rules_v = {
@@ -83,7 +83,7 @@ def get_phonetic_rules(region: str, is_tvm: bool = False) -> tuple:
                 'p': ['t', 'k', 'ʒ', 'ç', 'f', 's', 'ş', 'x', 'h'],
                 'b': ['b', 'd', 'g', 'ž', 'c', 'v', 'z', 'j', 'ğ'],
                 'p̌': ['ç̌', 'ǩ', 'q', 'ǯ', 't̆'],
-                'm': ['n']
+                'm': ['n', 'm']
             }
         
         phonetic_rules_g = {
