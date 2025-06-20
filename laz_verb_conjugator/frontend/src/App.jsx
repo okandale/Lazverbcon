45f1ebd2b57670c04f9729c95304dd0a915938ef
Geoffrey ROYER <geoffrey.royer@gmail.com>
2025-06-20 11:41:08 +0300

    [WIP] Admin add verb

--- laz_verb_conjugator/frontend/src/App.jsx
+++ laz_verb_conjugator/frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import VerbConjugator from './components/VerbConjugator';
-import VerbList from './components/VerbList';
-import Classes from './components/Classes';
-import About from './components/About';
-import HomePage from './components/Home';
-import Resources from './components/Resources';
-import Keyboard from './components/Keyboard';
-import KeyboardWin from './components/Keyboardwin';       
-import KeyboardPC from './components/KeyboardPC';
-import KeyboardMAC from './components/KeyboardMAC';
-import KeyboardAndr from './components/KeyboardAndr';
-import KeyboardiPhone from './components/KeyboardiPhone';
-import KeyboardMobile from './components/KeyboardMobile';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import VerbConjugator from "./components/VerbConjugator";
+import VerbList from "./components/VerbList";
+import Classes from "./components/Classes";
+import About from "./components/About";
+import HomePage from "./components/Home";
+import Resources from "./components/Resources";
+import Keyboard from "./components/Keyboard";
+import KeyboardWin from "./components/Keyboardwin";
+import KeyboardPC from "./components/KeyboardPC";
+import KeyboardMAC from "./components/KeyboardMAC";
+import KeyboardAndr from "./components/KeyboardAndr";
+import KeyboardiPhone from "./components/KeyboardiPhone";
+import KeyboardMobile from "./components/KeyboardMobile";
 import AdminAuth from "./components/AdminAuth";
 import RequireAuth from "./components/RequireAuth";
 import AdminPanel from "./components/AdminPanel";
@@ -19,7 +19,8 @@ import AdminLogout from "./components/AdminLogout";
 import PickVerbForm from "./components/v2/PickVerbForm";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import VerbDetails from "./components/v2/VerbDetails";
-
+import AdminManageVerbs from "./components/admin/AdminManageVerbs";
+import AddVerb from "./components/admin/AddVerb";
 
 const theme = createTheme({
   palette: {
@@ -56,44 +57,58 @@ const theme = createTheme({
 
 function App() {
   return (
-  <ThemeProvider theme={theme}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/conjugator" element={<VerbConjugator />} />
-        <Route path="/verbs" element={<VerbList />} />
-        <Route path="/events" element={<Classes />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/keyboard" element={<Keyboard />} />
-        <Route path="/keyboard/windows" element={<KeyboardWin />} />
-        <Route path="/keyboard/computer" element={<KeyboardPC />} />
-        <Route path="/keyboard/mac" element={<KeyboardMAC />} />
-        <Route path="/keyboard/android" element={<KeyboardAndr />} />
-        <Route path="/keyboard/iphone" element={<KeyboardiPhone />} />
-        <Route path="/keyboard/phone" element={<KeyboardMobile />} />
-        <Route path="/admin" element={<AdminAuth />} />
-        <Route
-          path="/admin/panel"
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/conjugator" element={<VerbConjugator />} />
+          <Route path="/verbs" element={<VerbList />} />
+          <Route path="/events" element={<Classes />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/keyboard" element={<Keyboard />} />
+          <Route path="/keyboard/windows" element={<KeyboardWin />} />
+          <Route path="/keyboard/computer" element={<KeyboardPC />} />
+          <Route path="/keyboard/mac" element={<KeyboardMAC />} />
+          <Route path="/keyboard/android" element={<KeyboardAndr />} />
+          <Route path="/keyboard/iphone" element={<KeyboardiPhone />} />
+          <Route path="/keyboard/phone" element={<KeyboardMobile />} />
+          <Route path="/admin" element={<AdminAuth />} />
+          <Route
+            path="/admin/panel"
+            element={
+              <RequireAuth>
+                <AdminPanel />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/admin/logout"
+            element={
+              <RequireAuth>
+                <AdminLogout />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/admin/manage-verbs"
+            element={
+              <RequireAuth>
+                <AdminManageVerbs />
+              </RequireAuth>
+            }
+          />
+          <Route path="/admin/add-verb"
           element={
             <RequireAuth>
-              <AdminPanel />
+              <AddVerb />
             </RequireAuth>
-          }
-        />
-        <Route
-          path="/admin/logout"
-          element={
-            <RequireAuth>
-              <AdminLogout />
-            </RequireAuth>
-          }
-        />
-        <Route path="/v2/verbs" element={<PickVerbForm />} />
-          <Route path="/v2/verb/:verbID/:verbType" element = {<VerbDetails />} />
-      </Routes>
-    </Router>
-  </ThemeProvider>
+          } />
+          <Route path="/v2/verbs" element={<PickVerbForm />} />
+          <Route path="/v2/verb/:verbID/:verbType" element={<VerbDetails />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ laz_verb_conjugator/frontend/src/components/v2/RegionSelector.jsx
@@ -0,0 +1,59 @@
+import Chip from "@mui/material/Chip";
+import { useState } from "react";
+
+import {
+    regionColors
+} from "../constants";
+
+const RegionSelector = ({initialSelectedRegions}) => {
+
+  const [selectedRegions, setSelectedRegions] = useState(initialSelectedRegions);
+
+  const regions = [
+    {
+        "code": "AS",
+        "name": "Ardeşen",
+    },
+    {
+        "code": "HO",
+        "name": "Hopa"
+    },
+    {
+        "code": "FA",
+        "name": "Fındıklı-Arhavi"
+    },
+    {
+        "code": "PZ",
+        "name": "Pazar"
+    }
+  ]
+
+  const onChipClick = (regionCode) => {
+    const newSet = new Set(selectedRegions);
+    if (newSet.has(regionCode)) {
+        newSet.delete(regionCode);
+    } else {
+        newSet.add(regionCode);
+    }
+    setSelectedRegions(newSet);
+  }
+
+  return (
+    <div className="flex gap-1">
+      {regions.map((region) => (
+        <Chip
+          key={region["code"]}
+          label={region["name"]}
+          color={regionColors[region["code"]]}
+          onClick={() => onChipClick(region["code"])}
+           variant={selectedRegions.has(region["code"]) ? "filled" : "outlined"}
+        sx={{
+          opacity: selectedRegions.has(region["code"]) ? 1 : 0.2,
+        }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default RegionSelector;
